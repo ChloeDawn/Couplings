3dc1dd15914c35f576f781a92e0d33d132d20662
Chloe Dawn <admin@designdevils.co.uk>
2019-05-07 01:20:22 +0100

    Rewrite options implementation, remove logging, remove loaded check

--- src/main/java/io/github/insomniakitten/couplings/Couplings.java
+++ src/main/java/io/github/insomniakitten/couplings/Couplings.java
@@ -4,7 +4,6 @@ import com.google.common.base.MoreObjects;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
-import lombok.Cleanup;
 import lombok.SneakyThrows;
 import lombok.Value;
 import lombok.experimental.Accessors;
@@ -19,23 +18,14 @@ import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.annotation.Nullable;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.file.Path;
 
-import static com.google.common.base.Preconditions.checkState;
-
 public final class Couplings {
-  public static final int COUPLING_RANGE = 128;
-
-  private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
   private static final boolean DEVELOPMENT = Knot.getLauncher().isDevelopment();
   private static final Logger LOGGER = LogManager.getLogger("coupled");
-
-  @Nullable
-  private static Options options;
 
   private Couplings() {
     throw new UnsupportedOperationException();
@@ -47,66 +37,48 @@ public final class Couplings {
     final Path configs = FabricLoader.getInstance().getConfigDirectory().toPath();
     final File config = configs.resolve("couplings.json").toFile();
     if (config.exists()) {
-      @Cleanup final FileReader reader = new FileReader(config);
-      final Options options = Couplings.GSON.fromJson(reader, Options.class);
-      checkState(options != null, "Null config %s", config);
-      Couplings.options = options;
-    } else {
-      if (config.createNewFile()) {
-        final Options.Features features = new Options.Features(true, true, true);
-        Couplings.options = new Options(true, Couplings.COUPLING_RANGE, features);
-        @Cleanup final FileWriter writer = new FileWriter(config);
-        writer.append(Couplings.GSON.toJson(Couplings.options));
+      try (final FileReader reader = new FileReader(config)) {
+        final Options options = Options.fromJson(reader);
+        if (options == null) {
+          Couplings.LOGGER.error("Invalid config: {}", config);
+        } else {
+          Options.instance = options;
+          return;
+        }
       }
+    }
+    try (final FileWriter writer = new FileWriter(config)) {
+      writer.append(Options.toJson());
     }
   }
 
   public static boolean requiresNoSneaking() {
-    return !Couplings.options().ignoreSneaking();
+    return !Options.instance.ignoreSneaking();
   }
 
   public static int getCouplingRange() {
-    return Couplings.options().couplingRange();
+    return Options.instance.couplingRange();
   }
 
   public static boolean areDoorsEnabled() {
-    return Couplings.options().enabledFeatures().areDoorsEnabled();
+    return Options.instance.enabledFeatures().areDoorsEnabled();
   }
 
   public static boolean areFenceGatesEnabled() {
-    return Couplings.options().enabledFeatures().areFenceGatesEnabled();
+    return Options.instance.enabledFeatures().areFenceGatesEnabled();
   }
 
   public static boolean areTrapdoorsEnabled() {
-    return Couplings.options().enabledFeatures().areTrapdoorsEnabled();
+    return Options.instance.enabledFeatures().areTrapdoorsEnabled();
   }
 
   public static boolean isUsable(final World world, final BlockPos pos, final PlayerEntity player) {
-    if (!player.canModifyWorld()) {
-      if (Couplings.DEVELOPMENT) {
-        Couplings.LOGGER.warn("Skipping usage as world modification is disallowed for user {}", player);
-      }
-      return false;
-    }
-    if (!world.isBlockLoaded(pos)) {
-      if (Couplings.DEVELOPMENT) {
-        Couplings.LOGGER.warn("Skipping usage as block is unloaded at {}", pos);
-      }
-      return false;
-    }
-    if (!world.getWorldBorder().contains(pos)) {
-      if (Couplings.DEVELOPMENT) {
-        Couplings.LOGGER.warn("Skipping usage as block is outside of the world border at {}", pos);
-      }
-      return false;
-    }
-    return true;
+    return player.canModifyWorld() && world.getWorldBorder().contains(pos);
   }
 
   public static void use(final BlockState state, final BlockState other, final World world, final Hand hand, final PlayerEntity player, final BlockHitResult origin, final BlockPos offset, final boolean usageResult) {
     final BlockHitResult target = new BlockHitResult(origin.getPos(), origin.getSide(), offset, false);
-    final boolean otherUsageResult = other.activate(world, player, hand, target);
-    if (usageResult != otherUsageResult) {
+    if (usageResult != other.activate(world, player, hand, target)) {
       final String result1 = Couplings.toString(world, player, hand, state, origin, usageResult);
       final String result2 = Couplings.toString(world, player, hand, other, target, usageResult);
       if (Couplings.DEVELOPMENT) {
@@ -126,15 +98,14 @@ public final class Couplings {
       .toString();
   }
 
-  private static Options options() {
-    final Options options = Couplings.options;
-    checkState(options != null, "Options not loaded");
-    return options;
-  }
-
   @Value
   @Accessors(fluent = true)
   private static final class Options {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Options DEFAULT = new Options(true, 128, Features.DEFAULT);
+
+    private static Options instance = Options.DEFAULT;
+
     @SerializedName("ignore_sneaking")
     private final boolean ignoreSneaking;
 
@@ -142,10 +113,20 @@ public final class Couplings {
     private final int couplingRange;
 
     @SerializedName("enabled_features")
-    private final Options.Features enabledFeatures;
+    private final Features enabledFeatures;
+
+    private static Options fromJson(final FileReader reader) {
+      return Options.GSON.fromJson(reader, Options.class);
+    }
+
+    private static String toJson() {
+      return Options.GSON.toJson(Options.instance);
+    }
 
     @Value
-    public static final class Features {
+    private static final class Features {
+      private static final Features DEFAULT = new Features(true, true, true);
+
       @SerializedName("doors")
       private final boolean areDoorsEnabled;
 
