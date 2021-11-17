747c5bf6c561962620241cd5d6325c5fc397f1a6
Chloe Dawn <chloe@sapphic.dev>
2021-11-17 18:58:11 +0000

    Remove ContainerListener from ServerPlayerMixin
    It is no longer a part of the hierarchy, and is causing Mixin to
    create technically invalid bytecode as it leaves unimplemented
    abstract methods

--- src/main/java/dev/sapphic/couplings/mixin/ServerPlayerMixin.java
+++ src/main/java/dev/sapphic/couplings/mixin/ServerPlayerMixin.java
@@ -21,13 +21,12 @@ import dev.sapphic.couplings.CouplingsPlayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.ContainerListener;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(ServerPlayer.class)
-abstract class ServerPlayerMixin extends Player implements ContainerListener, CouplingsPlayer {
+abstract class ServerPlayerMixin extends Player implements CouplingsPlayer {
   @Unique private boolean couplingIgnoresSneaking = true;
 
   ServerPlayerMixin(final Level level, final BlockPos pos, final float spawnAngle, final GameProfile profile) {
